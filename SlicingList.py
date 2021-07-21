109edcfe43125c13edc7c141bc717b951e1b1d06
rahul <rahuljha12527@bitbucket.org>
2021-07-21 05:59:38 +0530

    understaning slicing

---
+++ SlicingList.py
@@ -0,0 +1,26 @@
+l1=[10,20,30,40,50]
+
+l2=l1[:]
+# print(l[0:5:2])
+
+# print(l[:4])
+
+# print(l[2:])
+
+# print(l[1:4])
+
+# print(l[4:1:-1])
+
+t1=(10,20,30)
+t2=t1[:]
+
+
+s1="geeks"
+s2=s1[:]
+
+print(l1 is l2)
+
+print(t1 is t2)
+
+
+print(s1 is s2)
