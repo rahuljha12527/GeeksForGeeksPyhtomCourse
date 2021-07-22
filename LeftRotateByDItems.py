6973ad09804133682b0c9196f6e5df565ada65e8
rahul <rahuljha12527@bitbucket.org>
2021-07-22 22:31:55 +0530

    left rotate by d

---
+++ LeftRotateByDItems.py
@@ -0,0 +1,66 @@
+
+
+#########################################
+
+########### Using Direct Method
+
+l=[10,20,30,40,50]
+d=2
+l=l[d:]+l[:d]
+print(l)
+
+
+###########################################################
+
+###################### Another Mehtod
+
+## TimeComplecty O(n)
+
+## Auxilary Space O(1)
+
+
+
+l=[10,20,30,40,50]
+def leftRotate(l,d):
+    for i in range(0,d):
+        l.append(l.pop(0))
+
+
+d=2
+leftRotate(l,d)
+print(l)
+
+
+
+
+
+
+
+####################################################################
+
+l=[10,20,30,40,50]
+d=2
+def leftRotates(l,d):
+
+    n=len(l)
+    reverse(l,0,d-1)
+    reverse(l,d,n-1)
+    reverse(l,0,n-1)
+
+
+
+def reverse(l,a,b):
+
+    while a<b:
+
+        l[a],l[b]=l[b],l[a]
+        a=a+1
+        b=b-1
+    return l
+
+
+
+
+
+leftRotates(l,d)
+print(l)
