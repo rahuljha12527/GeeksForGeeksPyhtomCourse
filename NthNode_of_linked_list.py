e0699def96558ed2c670c1cf0abcaa0b9faf3290
rahul <rahuljha12527@bitbucket.org>
2021-08-28 23:25:10 +0530

    find nth of linked list

--- NthNode_of_linked_list.py
+++ NthNode_of_linked_list.py
@@ -28,4 +28,30 @@ def getNthFromLast(head,n):
     
     
     return -1
+    
+    ##################### geeksgeeksforgeeks approach
+
+    def getNthFromLast(head,n):
+    
+    #using two pointers, similar to finding middle element.
+    curr_node = head
+    nth_node = head
+
+    #traversing first n elements with first pointer.
+    while n :
+        if n and curr_node == None:
+            return -1
+        curr_node = curr_node.next
+        n-=1
+
+    #now traversing with both pointers and when first pointer gives null 
+    #we have got the nth node from end in second pointer since the first 
+    #pointer had already traversed n nodes and thus had difference of n 
+    #nodes with second pointer.
+    while curr_node :
+        curr_node = curr_node.next
+        nth_node = nth_node.next
+
+    #returning the data of nth node from end.
+    return nth_node.data
         
